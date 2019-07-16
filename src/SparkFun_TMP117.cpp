cb5f797b4facf963012393c407af516f0ce1e8ae
Madison Chodikov <mach5813@colorado.edu>
2019-07-16 09:34:52 -0600

    Fixed version numbers

--- src/SparkFun_TMP117.cpp
+++ src/SparkFun_TMP117.cpp
@@ -12,7 +12,7 @@ settings in the sensor.
 Development environment specifics:
 	IDE: Arduino 1.8.9
 	Hardware Platform: Arduino Uno
-	TMP117 Breakout Version: 1.0
+	TMP117 Breakout Version: 1.0.0
 
 This code is beerware; if you see me (or any other SparkFun employee) at the
 local, and you've found our code helpful, please buy us a round!
--- src/SparkFun_TMP117_Registers.h
+++ src/SparkFun_TMP117_Registers.h
@@ -10,7 +10,7 @@ This file defines all registers internal to the TMP117 sensor.
 Development environment specifics:
 	IDE: Arduino 1.8.9
 	Hardware Platform: Arduino Uno
-	TMP117 Breakout Version: 1.0
+	TMP117 Breakout Version: 1.0.0
 
 This code is beerware; if you see me (or any other SparkFun employee) at the
 local, and you've found our code helpful, please buy us a round!
